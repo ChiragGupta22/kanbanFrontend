d69b287da2db43399443d1494db25a506bb8632c
Chirag <chiragguptaworks@gmail.com>
2026-05-03 16:43:26 +0530

    links

--- src/services/ws.js
+++ src/services/ws.js
@@ -4,7 +4,7 @@ let socket = null;
 
 export const connectWS = () => {
   if (!socket) {
-    socket = io("http://localhost:5000", {
+    socket = io("https://kanbanbackend-2-x4y5.onrender.com", {
       withCredentials: true,
     });
   }
